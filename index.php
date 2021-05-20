52ee458d5c8f8591935dd3cc3803a8617f8ed653
Souvik Ghosh <souvikghosh2701@gmail.com>
2021-05-20 21:52:22 +0530

    Trial1
    Created user.ini file to control php.ini
    edited index.php - trying to handle sessions

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-  $hi = "Hello World";  
+  session_start();
+  $hi = "Hello World <br>";
   echo $hi;
+
+  if(isset($_SESSION['user']))
+  {
+    $user = $_SESSION['user'];
+    echo "Session is set. Output is - " . $user;
+  } else {
+    echo "Session not set";
+    $_SESSION['user'] = "John Doe";
+  }
 ?>
